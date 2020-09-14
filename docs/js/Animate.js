826b9f625233c8bc33f8068705745d12865ff8aa
Hannah Hamavid <hamavid@Hannahs-computer.local>
2020-09-13 17:02:55 -0700

    moving some functions around in animatejs

--- docs/js/Animate.js
+++ docs/js/Animate.js
@@ -10,58 +10,69 @@ $(document).ready(function(){
 	d3.svg("./svgs/web_outline_three_pieces.svg").then(function(xml) {
 		d3.select("div#pb-borders").node().appendChild(xml.documentElement);
 	});
+
+//SWITCH THINGS UP 
+	scrollControl();
+	$('#wantmorelink').click(scrollControl);
+	$(window).bind('scroll resize', scrollControl);
 	
 // function to get window info
 	function getwindowinfo() {
 		var titletop = document.getElementById('title-pocket').getBoundingClientRect().top;
-		//var wantmoretop = document.getElementById('wantmore').getBoundingClientRect().top;
 		var bigonetop = document.getElementById('bigone').getBoundingClientRect().top;
 		var bigoneheight = document.getElementById('bigone').clientHeight;
-		//console.log(bigoneheight);
-		var vh = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-		//var vw = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 		var bannerthresh = 61;
 		return {
 	        titletop: titletop,
 	        bigonetop: bigonetop,
 	        bigoneheight: bigoneheight,
-	        //wantmoretop: wantmoretop,
-	        vh: vh,
 	        bannerthresh: bannerthresh
 	    };
 	}
 
-	// Do stuff when page is fully loaded + a bit just in case
-	$(window).load( function(){ 
-		setTimeout( function(){
-			// set vars
-			var puzza = $('div#pa');
-			var puzzbstill = $('div#pb>div>svg>g>g.still');
-			var puzzbanim = $('div#pb>div>svg>g>g.animateme');
-			var puzzbborder = $('div#pb-borders');
-			var lvl2 = $('path#lvl2');
-			var lvl1 = $('path#lvl1');
-			var lvl0 = $('path#lvl0');
-			var banner = $('div#banner');
-			var slider = $('div#slider');
+// set variables 
+	function setvars() {
+		var puzza = $('div#pa');
+		var puzzbstill = $('div#pb>div>svg>g>g.still');
+		var puzzbanim = $('div#pb>div>svg>g>g.animateme');
+		var puzzbborder = $('div#pb-borders');
+		var lvl2 = $('path#lvl2');
+		var lvl1 = $('path#lvl1');
+		var lvl0 = $('path#lvl0');
+		var banner = $('div#banner');
+		var slider = $('div#slider');
+		return{
+			puzza: puzza,
+			puzzbstill: puzzbstill,
+			puzzbanim: puzzbanim,
+			puzzbborder: puzzbborder,
+			lvl2: lvl2,
+			lvl1: lvl1,
+			lvl0: lvl0,
+			banner: banner,
+			slider: slider
+		};
+	}
 
-			// show puzzle a
-			puzza.css('opacity',1);
 
-			//SWITCH THINGS UP 
-			scrollControl();
-			$('#wantmorelink').click(scrollControl);
-			$(window).bind('scroll resize', scrollControl);
-			function scrollControl() {
+	function scrollControl() {
 
-				// get window info
-				var windowinfo = getwindowinfo();
-				var titletop = windowinfo.titletop;
-				var bigonetop = windowinfo.bigonetop;
-				var bigoneheight = windowinfo.bigoneheight;
-				//var wantmoretop = windowinfo.wantmoretop;
-				//var vh = windowinfo.vh;
-				var bannerthresh = windowinfo.bannerthresh;
+		// get vars
+			var wi = getwindowinfo();
+			var titletop = wi.titletop;
+			var bigonetop = wi.bigonetop;
+			var bigoneheight = wi.bigoneheight;
+			var bannerthresh = wi.bannerthresh;
+			var sv = setvars();
+			var puzza = sv.puzza;
+			var puzzbstill = sv.puzzbstill;
+			var puzzbanim = sv.puzzbanim;
+			var puzzbborder = sv.puzzbborder;
+			var lvl2 = sv.lvl2;
+			var lvl1 = sv.lvl1;
+			var lvl0 = sv.lvl0;
+			var banner = sv.banner;
+			var slider = sv.slider;
 
 			// PUZZLES
 				// Switch from puzzle a to b when a is solved
@@ -74,11 +85,7 @@ $(document).ready(function(){
 				else{puzzbborder.css('opacity',0);lvl2.css('opacity',0);}
 				if (bigonetop < -0.76 * bigoneheight) {lvl1.css('opacity',1);}else{lvl1.css('opacity',0);}
 				if (bigonetop < -0.79 * bigoneheight) {lvl0.css('opacity',1);}else{lvl0.css('opacity',0);}
-				// TO FIX: THIS IS INCONSISTENT ON MOBILE BROWSERS
-				/*if (wantmoretop < 2.78 * vh) {lvl2.css('opacity',1);}else{lvl2.css('opacity',0);}
-				if (wantmoretop < 2.33 * vh) {lvl1.css('opacity',1);}else{lvl1.css('opacity',0);}
-				if (wantmoretop < 1.66 * vh) {lvl0.css('opacity',1);}else{lvl0.css('opacity',0);}*/
-			
+				
 			// BANNER/SLIDER
 				// Switch from slider to banner when top of text pocket hits where bottom of banner will be on its way in
 				if (titletop <= bannerthresh) {
@@ -96,6 +103,14 @@ $(document).ready(function(){
 				}
 			}
 
+
+	// Do stuff when page is fully loaded + a bit just in case
+	$(window).load( function(){ 
+		setTimeout( function(){
+			// show puzzle a
+			$('div#pa').css('opacity',1);
+			scrollControl();
+			
 		} , 100 ) 
 
 	});
